1b60882c8dbd9ad89e1280c9378ace5ee1edb759
Sean Knox <knoxville@microsoft.com>
2018-04-12 17:31:39 -0700

    (terraform): add post TF steps

--- terraform/modules/controller/prepare_controller.sh
+++ terraform/modules/controller/prepare_controller.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -eu
 
-KUBE_API_INTERNAL_IP=$1
-BOOTSTRAP_TOKEN=$2
-
-IFS=. read TOKEN_ID TOKEN_CONTENT <<< $BOOTSTRAP_TOKEN
-TOKENID_BASE64=$(base64 <<< $TOKEN_ID)
-TOKEN_CONTENT_BASE64=$(base64 <<< $TOKEN_CONTENT)
-
 # create SSL dirs
 mkdir -p /etc/kubernetes/ssl
 mkdir -p /etc/kubernetes/manifests
