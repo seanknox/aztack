ccbc8039eb1e43d21b602bf265a212b2cec3e9dd
Sean Knox <knoxville@gmail.com>
2018-05-05 14:18:20 +0200

    (nodes): reenable TLS node bootstrap

--- terraform/modules/node/prepare_node.sh
+++ terraform/modules/node/prepare_node.sh
@@ -20,20 +20,20 @@ sudo kubectl config set-cluster kubernetes \
   --certificate-authority=/etc/kubernetes/ssl/ca.pem \
   --embed-certs=true \
   --server=https://${KUBE_API_INTERNAL_IP}:6443 \
-  --kubeconfig=/etc/kubernetes/kubelet.kubeconfig
+	--kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf
 
-sudo kubectl config set-credentials system:node:${HOSTNAME} \
-  --client-certificate=/var/lib/kubelet/pki/kubelet.crt \
-  --client-key=/var/lib/kubelet/pki/kubelet.key \
-  --embed-certs=true \
-  --kubeconfig=/etc/kubernetes/kubelet.kubeconfig
+sudo kubectl config set-credentials \
+  tls-bootstrap-token-user --token=${BOOTSTRAP_TOKEN} \
+  --user=tls-bootstrap-token-user \
+  --kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf
+
 
 sudo kubectl config set-context default \
   --cluster=kubernetes \
-	--user=system:node:${HOSTNAME} \
-  --kubeconfig=/etc/kubernetes/kubelet.kubeconfig
+  --user=tls-bootstrap-token-user \
+  --kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf
 
-sudo kubectl config use-context default --kubeconfig=/etc/kubernetes/kubelet.kubeconfig
+sudo kubectl config use-context default --kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf
 
 ## Setup kube-proxy
 kubectl config set-cluster kubernetes \
