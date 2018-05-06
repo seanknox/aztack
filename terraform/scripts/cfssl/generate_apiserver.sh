042485188e11c1f09c8be0effa62d1910067f5f1
Sean Knox <knoxville@gmail.com>
2018-05-06 22:57:04 +0200

    (TLS): add kubernetes API IP to cert hostname

--- terraform/scripts/cfssl/generate_apiserver.sh
+++ terraform/scripts/cfssl/generate_apiserver.sh
@@ -8,5 +8,5 @@ CFSSL_DIR=$(dirname "${BASH_SOURCE[0]}")
 cfssl gencert -ca=$SECRETS_DIR/ca.pem \
     -ca-key=$SECRETS_DIR/ca-key.pem \
     -config=$CFSSL_DIR/ca-config.json \
-	-hostname=etcd1.${INTERNAL_TLD},etcd2.${INTERNAL_TLD},etcd3.${INTERNAL_TLD},${ETCD_IPS},${MASTER_IPS},controller1.${INTERNAL_TLD},controller2.${INTERNAL_TLD},controller3.${INTERNAL_TLD},${KUBE_API_PUBLIC_FQDN},${KUBE_API_INTERNAL_IP},127.0.0.1,kubernetes.default \
+	-hostname=etcd1.${INTERNAL_TLD},etcd2.${INTERNAL_TLD},etcd3.${INTERNAL_TLD},${ETCD_IPS},${MASTER_IPS},controller1.${INTERNAL_TLD},controller2.${INTERNAL_TLD},controller3.${INTERNAL_TLD},${KUBE_API_PUBLIC_FQDN},${KUBE_API_INTERNAL_IP},10.3.0.1,127.0.0.1,kubernetes.default \
     -profile=kubernetes $CFSSL_DIR/kube-apiserver-csr.json | cfssljson -bare $SECRETS_DIR/kube-apiserver
