0538dbe0ee3e83e53638e10d088dad360fc64de6
Sean Knox <knoxville@gmail.com>
2018-05-10 10:22:15 +0200

    (image): copy source image VHD to storage account in users resource group

--- terraform/modules/image/image.tf
+++ terraform/modules/image/image.tf
@@ -1,3 +1,27 @@
+resource "azurerm_storage_account" "acstack" {
+  name                     = "acstack${ var.name }"
+  resource_group_name      = "${ var.resource_group_name }"
+  location                 = "${ var.location }"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "acstack" {
+  name                  = "vhds"
+  resource_group_name   = "${ var.resource_group_name }"
+  storage_account_name  = "${ azurerm_storage_account.acstack.name }"
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "acstack" {
+  name = "acstack.vhd"
+
+  resource_group_name    = "${ var.resource_group_name }"
+  storage_account_name   = "${ azurerm_storage_account.acstack.name }"
+  storage_container_name = "${ azurerm_storage_container.acstack.name }"
+  source_uri             = "${ var.azure_vhd_uri }"
+}
+
 resource "azurerm_image" "acs" {
   name                = "${ var.name }-image"
   location            = "${ var.location }"
@@ -6,7 +30,7 @@ resource "azurerm_image" "acs" {
   os_disk {
     os_type  = "Linux"
     os_state = "Generalized"
-    blob_uri = "${ var.azure_vhd_uri }"
+    blob_uri = "${ azurerm_storage_blob.acstack.url }"
     size_gb  = 30
   }
 }
