ef38fb06ca8a6c9ba2e781406b24c15a7abec906
Sean Knox <knoxville@gmail.com>
2018-05-21 10:16:27 -0700

    (terraform): Dockerize it all

--- terraform/makefiles/terraform.mk
+++ terraform/makefiles/terraform.mk
@@ -1,7 +1,4 @@
-.terraform: ; terraform get
-
-terraform.tfvars:
-	@./scripts/init-variables
+.terraform: ; cd $(TERRAFORM_DIR) && terraform get
 
 module.%:
 	@echo "${BLUE}❤ make $@ - commencing${NC}"
@@ -12,29 +9,29 @@ module.%:
 ## terraform apply
 apply: plan
 	@echo "${BLUE}❤ terraform apply - commencing${NC}"
-	terraform apply -state-out=build/$(CLUSTER_NAME)/terraform.tfstate build/$(CLUSTER_NAME)/terraform.tfplan
+	terraform apply -state-out=$(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfstate $(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfplan
 	@echo "${GREEN}✓ make $@ - success${NC}"
 
 ## terraform destroy
 destroy:
 	@echo "${RED}****** DESTRUCTIVE ACTION AHEAD! ******${NC}"
-	@./scripts/delete_cluster_resource_groups
+	$(DOCKER_CODE_PATH)/scripts/delete_cluster_resource_groups
 
 ## terraform get
 get: ; terraform get
 
 ## generate variables
-init: terraform.tfvars
+init:
+	@cd $(TERRAFORM_DIR) && $(DOCKER_CODE_PATH)/scripts/init-variables
 
 ## terraform plan
 plan: get
-	cd build/$(CLUSTER_NAME)
 	terraform init
-	terraform validate -var-file=build/$(CLUSTER_NAME)/terraform.tfvars
+	terraform validate -var-file=$(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfvars
 	@echo "${GREEN}✓ terraform validate - success${NC}"
-	terraform plan -state=build/$(CLUSTER_NAME)/terraform.tfstate -var-file=build/$(CLUSTER_NAME)/terraform.tfvars -out build/$(CLUSTER_NAME)/terraform.tfplan
+	terraform plan -state=$(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfstate -var-file=$(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfvars -out $(TERRAFORM_DIR)/$(CLUSTER_NAME)/terraform.tfplan
 
 ## terraform show
-show: ; terraform show
+show: ; cd $(TERRAFORM_DIR) && terraform show
 
-.PHONY: apply destroy get init module.% plan show
+.PHONY: apply destroy get init module.% plan show foo
