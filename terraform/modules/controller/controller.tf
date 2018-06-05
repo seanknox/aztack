5d7c00d1b3a1b7d22988ca0dc1cdb080d6df84c1
Sean Knox <knoxville@gmail.com>
2018-06-04 21:13:06 -0700

    chore(controller/node): don't create data disks

--- terraform/modules/controller/controller.tf
+++ terraform/modules/controller/controller.tf
@@ -111,15 +111,6 @@ resource "azurerm_virtual_machine" "controller" {
     managed_disk_type = "Standard_LRS"
   }
 
-  # Optional data disks
-  storage_data_disk {
-    name              = "controllerdatadisk${ count.index + 1 }"
-    managed_disk_type = "Standard_LRS"
-    create_option     = "Empty"
-    lun               = 0
-    disk_size_gb      = "1023"
-  }
-
   os_profile {
     computer_name  = "controller${ count.index + 1 }.${ var.internal-tld }"
     admin_username = "ubuntu"
--- terraform/modules/node/node.tf
+++ terraform/modules/node/node.tf
@@ -65,15 +65,6 @@ resource "azurerm_virtual_machine" "node" {
     managed_disk_type = "Standard_LRS"
   }
 
-  # Optional data disks
-  storage_data_disk {
-    name              = "nodedatadisk${ count.index + 1 }"
-    managed_disk_type = "Standard_LRS"
-    create_option     = "Empty"
-    lun               = 0
-    disk_size_gb      = "1023"
-  }
-
   os_profile {
     computer_name  = "node${ count.index + 1 }"
     admin_username = "ubuntu"
